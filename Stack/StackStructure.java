a072768eab18dc68e59d5bace7370583fcb7fc10
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-11-18 19:06:20 -0300

    Pesquisado a teória

--- Stack/StackStructure.java
+++ Stack/StackStructure.java
@@ -1,5 +1,15 @@
 package Stack;
 
+/*** A estrutura de dados Stack ela é LIFO(Last in first Out), ou seja o ultimo elemento a entrar é o primeiro a sair. 
+ * O tipo que vou fazer é numérico
+ * ela vai ter os seguintes metodos padrões:
+ * @push: método que adiciona um novo item no stack
+ * @pop: método que remove o ultimo elemento no stack
+ * @peek: método que vai me dizer o ultimo elemento da stack
+ * @isEmpty: método que vai me dizer se a stack está vazia ou não
+ * @size: método que retorna a quantidade de elemento no stack.
+ */
+
 public class StackStructure {
     
 }
