43f36d7b3a57b64c166bb6bf93f24a49346ae895
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-11-18 19:18:27 -0300

    Feito a classe da estrutura de dados

--- Stack/StackStructure.java
+++ Stack/StackStructure.java
@@ -1,5 +1,7 @@
 package Stack;
 
+import java.util.ArrayList; 
+
 /*** A estrutura de dados Stack ela é LIFO(Last in first Out), ou seja o ultimo elemento a entrar é o primeiro a sair. 
  * O tipo que vou fazer é numérico
  * ela vai ter os seguintes metodos padrões:
@@ -11,5 +13,27 @@ package Stack;
  */
 
 public class StackStructure {
-    
+    private ArrayList<Integer> element = new ArrayList<Integer>();
+
+    void push(int elemento){
+        this.element.add(elemento);
+    }
+    void pop(){
+        this.element.removeLast();
+    }
+    void peek(){
+        this.element.getLast();
+    }
+    boolean isEmpty(){
+        int isEmpty = this.element.size();
+        if(isEmpty > 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    int sizeStack(){
+        return this.element.size();
+    }
 }
