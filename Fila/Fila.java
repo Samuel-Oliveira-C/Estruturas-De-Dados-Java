5754316eab2d2676e634fe48f6171c609cf4ee89
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-11-26 18:28:32 -0300

    Feito a teoria da fila

--- Fila/Fila.java
+++ Fila/Fila.java
@@ -1,5 +1,16 @@
 package Fila;
 
+/*** a Estrutura de dados Fila funciona com a ordem de acesso FIFO (First in, First out),
+ * ou seja o primeiro elementro a entrar na fila é o primeiro elemento a sair dela.
+ * metodo opcional : @isFull: Verifica se a fila está cheia (se houver um limite de elementos).
+ * metodos que vou criar:
+ * @Enfileiramento: vai adicionar um novo elemento no final da fila
+ * @Desenfileiramento: vai remover o ultimo elemento
+ * @Front: vai retorna o elemento no início da fila (sem removê-lo).
+ * @Rear: Retorna o elemento no final da fila (sem removê-lo).
+ * @isEmpty: Verifica se a fila está vazia.
+ */
+
 public class Fila {
     
 }
