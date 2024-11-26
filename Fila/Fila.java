d2f1124997464cd00446d1594ebba1ed98c1e6ec
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-11-26 18:37:29 -0300

    Feito a classe, hora de testar

--- Fila/Fila.java
+++ Fila/Fila.java
@@ -1,16 +1,34 @@
 package Fila;
 
+import java.util.ArrayList;
+
 /*** a Estrutura de dados Fila funciona com a ordem de acesso FIFO (First in, First out),
  * ou seja o primeiro elementro a entrar na fila é o primeiro elemento a sair dela.
  * metodo opcional : @isFull: Verifica se a fila está cheia (se houver um limite de elementos).
  * metodos que vou criar:
  * @Enfileiramento: vai adicionar um novo elemento no final da fila
- * @Desenfileiramento: vai remover o ultimo elemento
+ * @Desenfileiramento: vai remover o elemento do inicio 
  * @Front: vai retorna o elemento no início da fila (sem removê-lo).
  * @Rear: Retorna o elemento no final da fila (sem removê-lo).
  * @isEmpty: Verifica se a fila está vazia.
  */
-
 public class Fila {
-    
+    private ArrayList<Integer> fila = new ArrayList<Integer>(); //podia ser de outro tipo, mas para simplificar vou utilizar esse
+
+    void enfileiramento(int element){
+        fila.add(element);
+    }
+    void desenfileiramento (){
+        fila.remove(0);
+    }
+    void front(){
+        fila.get(0);
+    }
+    void rear(){
+        int lastElement = fila.size();
+        fila.get(lastElement + 1);
+    }
+    boolean isEmpty(){
+        return fila.isEmpty();
+    }
 }
