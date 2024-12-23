19c54b61ceb503f4e55d1ea61d11bbc3d58a3575
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-12-23 18:23:33 -0300

    Foi tirado o atributo do array, e adicionado metodos para a estrutura de dados

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,5 +1,34 @@
 package LinkedList;
 
+import java.util.ArrayList;
+
 public class LinkedList {
-    
+    private int node;
+    private LinkedList next;
+
+    public LinkedList(int node) {
+        this.node = node;
+        this.next = null;
+    }
+
+    public void iterarLinkedList(LinkedList node){
+        LinkedList atual = node;
+
+        while(atual != null){
+            System.out.println(atual.node + "");
+
+            atual = atual.next;
+        }
+        System.out.println("Não tem nenhum Nó");
+    }
+
+    public boolean procurarElemento(LinkedList node, int elementoProcurado ){
+        while(node != null){
+            if(node.node == elementoProcurado){
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
 }
