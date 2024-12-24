3a463a6aad2f122a306a7ab561b34fae6b750ab5
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-12-24 09:56:49 -0300

    Metodo de adicionar em alguma posição da lista ligada

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -64,4 +64,32 @@ public class LinkedList {
 
         return node;
     }
+    public LinkedList inserirPosicao (LinkedList node, int posicao , int elementoQueVaiInserir){
+        if(posicao < 1){
+            System.out.println("Posicão invalida");
+            return node;
+        }
+        if(posicao ==1 ){
+            LinkedList temp = new LinkedList(elementoQueVaiInserir);
+            temp.next = node;
+            return temp;
+        }
+        LinkedList anteriorElemento = node;
+        int count = 1;
+
+        while (count < posicao- 1 && anteriorElemento != null) {
+            anteriorElemento = anteriorElemento.next;
+            count ++;
+        }
+
+        if(anteriorElemento == null){
+            System.out.println("Posicão Invalida");
+            return node;
+        }
+        LinkedList temp = new LinkedList(elementoQueVaiInserir);
+        temp.next = anteriorElemento.next;
+        anteriorElemento.next = temp;
+
+        return node;
+    }
 }
